b5b00b1813731fc9eb7425764b7dcbada5153f1e
TheOGAltruist <staraknath@yahoo.co.in>
2024-12-23 18:09:00 +0000

    Changed status to statusCode for more accuracy

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use("/api", routes);
 // Simple error-handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
-  const status = err.status ?? 500;
+  const statusCode = err.statusCode ?? 500;
   const message = err.message ?? "Internal Server Error";
-  res.status(status).json({ message });
+  res.status(statusCode).json({ message });
 });
