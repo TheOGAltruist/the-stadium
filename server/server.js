d37c71de9e5fac5bc0f512f7521a7d5f89df0533
TheOGAltruist <staraknath@yahoo.co.in>
2024-12-23 18:57:37 +0000

    Changed the directory path to routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const routes = require("./routes");
+const routes = require("../server/api/routes");
 
 // Body-parsing middleware
 app.use(express.json());
