9924b35434ff5971b56e311777d33eaf0d3d8c5b
TheOGAltruist <staraknath@yahoo.co.in>
2024-12-27 01:26:15 +0000

    Added a route to get a specific user

--- server/api/routes/user.route.js
+++ server/api/routes/user.route.js
@@ -1,5 +1,6 @@
 const {
     getAllUsers,
+    getSpecificUser,
 } = require("../controllers/user.controller.js")
 
 const { isLoggedIn, isAdmin } = require("../utils/middleware.util.js")
@@ -7,4 +8,8 @@ const { isLoggedIn, isAdmin } = require("../utils/middleware.util.js")
 const router = require("express").Router()
 module.exports = router
 
+//All users
 router.get("/", isLoggedIn, isAdmin, getAllUsers)
+
+//Single user
+router.get("/:userId", isLoggedIn, isAdmin, getSpecificUser)
