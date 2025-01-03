760610d60a6d7319bbfe3024464b09c936925ee5
Andrew Yang <ayang312@gmail.com>
2025-01-02 21:38:39 -0500

    edited next() messages and change put to patch request

--- server/api/controllers/product.controller.js
+++ server/api/controllers/product.controller.js
@@ -6,7 +6,7 @@ const getAllProducts = async (req, res, next) => {
     const products = await prisma.product.findMany();
     res.status(200).json(products);
   } catch (error) {
-    next(error);
+    next({ message: "Failed to fetch all products" });
   }
 };
 
@@ -18,13 +18,13 @@ const getProductById = async (req, res, next) => {
     if (!product) return res.status(404).json({ error: "Product not found" });
     res.status(200).json(product);
   } catch (error) {
-    next(error);
+    next({ message: "Failed to fetch a product by ID" });
   }
 };
 
 // Create a new product (ADMIN ONLY)
 const createProduct = async (req, res, next) => {
-  const { name, description, price, quantity, tags, categories } = req.body;
+  const { name, description, price, quantity, skuId } = req.body;
   try {
     const newProduct = await prisma.product.create({
       data: {
@@ -32,22 +32,23 @@ const createProduct = async (req, res, next) => {
         description,
         price,
         quantity,
-        tags: { create: tags.map((tag) => ({ name: tag })) },
-        categories: {
-          create: categories.map((category) => ({ name: category })),
-        },
+        skuId,
       },
     });
-    res.status(201).json(newProduct);
+    res.status(201).json({
+      id: newProduct.id,
+      newProduct,
+      message: "Product created successfully",
+    });
   } catch (error) {
-    next(error);
+    next({ message: "Failed to create a product" });
   }
 };
 
 // Update a product (ADMIN ONLY)
 const updateProduct = async (req, res, next) => {
   const { id } = req.params;
-  const { name, description, price, quantity, tags, categories } = req.body;
+  const { name, description, price, quantity, skuId } = req.body;
   try {
     const product = await prisma.product.update({
       where: { id },
@@ -56,15 +57,16 @@ const updateProduct = async (req, res, next) => {
         description,
         price,
         quantity,
-        tags: { set: tags?.map((tag) => ({ name: tag })) },
-        categories: {
-          set: categories?.map((category) => ({ name: category })),
-        },
+        skuId,
       },
     });
-    res.status(200).json(product);
+    res.status(200).json({
+      id: product.id,
+      product,
+      message: "Product updated successfully",
+    });
   } catch (error) {
-    next(error);
+    next({ message: "Failed to update product" });
   }
 };
 
@@ -75,7 +77,7 @@ const deleteProduct = async (req, res, next) => {
     await prisma.product.delete({ where: { id } });
     res.status(204).send();
   } catch (error) {
-    next(error);
+    next({ message: "Failed to delete product" });
   }
 };
 
