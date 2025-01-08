0290027072d6f7a5374a33fdc4483b628146542c
Andrew Yang <ayang312@gmail.com>
2025-01-07 20:21:18 -0500

    imported getReviews fxn into review route

--- server/api/routes/review.route.js
+++ server/api/routes/review.route.js
@@ -1,20 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { getReviews } = require("../controllers/review.controller");
 
 // GET reviews for a product
-router.get('/:productId', getReviews);
-
+router.get("/:productId", getReviews);
 
 // Add a review for a product
 
-
-
 // Update a review
 
-
-
 // Delete a review
-
-
 
 module.exports = router;
