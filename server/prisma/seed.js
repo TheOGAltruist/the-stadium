056f8c21872965e45e13fa894eab2f32a82e154d
TheOGAltruist <staraknath@yahoo.co.in>
2025-01-08 00:23:18 +0000

    Added code for adding dummy data to the database

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,6 +1,58 @@
 const prisma = require(".");
+const { faker } = require("@faker-js/faker");
+const bcrypt = require("bcrypt")
+
 const seed = async () => {
-  //Actual data to be seeded
+
+  const createProducts = await prisma.product.createManyAndReturn({
+    data: [
+        {
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          price: 5.12,
+          quantity: 10,
+          skuId: faker.commerce.isbn(8)
+        },
+        {
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          price: 5.00,
+          quantity: 10,
+          skuId: faker.commerce.isbn(8)
+        },
+        {
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          price: 5.00,
+          quantity: 10,
+          skuId: faker.commerce.isbn(8)
+        },
+        {
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          price: 5.00,
+          quantity: 10,
+          skuId: faker.commerce.isbn(8)
+        },
+        {
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          price: 5.00,
+          quantity: 10,
+          skuId: faker.commerce.isbn(8)
+        },
+        {
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          price: 5.00,
+          quantity: 10,
+          skuId: faker.commerce.isbn(8)
+        }
+    ],
+    select: {
+        id: true
+    }
+  });
 };
 seed()
   .then(async () => await prisma.$disconnect())
