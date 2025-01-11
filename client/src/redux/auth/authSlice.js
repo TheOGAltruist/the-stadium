4475a1a1580cfa20d8ebe34e48c9e276a2de2300
Andrew Yang <ayang312@gmail.com>
2025-01-11 18:29:08 -0500

    cloned our project so far

---
+++ client/src/redux/auth/authSlice.js
@@ -0,0 +1,71 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: localStorage.getItem("token") || null,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    // trying to save token in redux store for immediate access
+    setToken(state, action) {
+      state.token = action.payload;
+    },
+    clearToken(state, action) {
+      state.token = null;
+      localStorage.removeItem("token");
+    },
+    loginStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess(state, action) {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    loginFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    registerStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    registerSuccess(state, action) {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    registerFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem("token");
+    },
+  },
+});
+
+export const {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  registerStart,
+  registerSuccess,
+  registerFailure,
+  logout,
+  setToken,
+  clearToken,
+} = authSlice.actions;
+export default authSlice.reducer;
