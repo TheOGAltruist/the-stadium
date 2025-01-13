958d15bb64812a3e6b5f4e695f0e7c1da54844b3
TheOGAltruist <staraknath@yahoo.co.in>
2025-01-13 20:16:07 +0000

    New file containing all admin routes. Contains routes for users, product, orders, wishlists, tags and categories

---
+++ server/api/routes/admin.route.js
@@ -0,0 +1,54 @@
+const {
+    getAllUsers,
+    getSpecificUser,
+    modifySpecificUser,
+} = require("../controllers/user.controller.js")
+
+const {
+    createProduct,
+    updateProduct,
+    deleteProduct,
+} = require("../controllers/product.controller.js")
+
+const {
+    getAllOrders,
+    changeOrderStatus,
+    createNewTag,
+    deleteTag,
+    createNewCategory,
+    deleteCategory,
+    getAllWishlists
+} = require("../controllers/admin.controller.js")
+
+const { isAdmin } = require("../middleware/middleware.util.js")
+
+const router = require("express").Router()
+module.exports = router
+
+//User related routes
+router.get("/users", isAdmin, getAllUsers) //Get All users
+router.post("/users", isAdmin) //Add new user
+router.get("/users/:userId", isAdmin, getSpecificUser) //Get specific user
+router.patch("/users/:userId", isAdmin, modifySpecificUser) //Update specific user
+
+//Product routes
+router.post("/products", isAdmin, createProduct); //Add new product
+router.patch("products/:id", isAdmin, updateProduct); //Update specific product
+router.delete("products/:id", isAdmin, deleteProduct); //Remove a product
+
+//Order routes
+router.get("/orders", isAdmin, getAllOrders) //Get all orders
+router.patch("/orders/:orderId", isAdmin, changeOrderStatus) //Change order status
+
+//Review routes
+
+//Tag routes
+router.post("/tags", isAdmin, createNewTag)
+router.delete("/tags/:tagId", isAdmin, deleteTag)
+
+//Category routes
+router.post("/categories", isAdmin, createNewCategory)
+router.delete("/categories/:categoryId", isAdmin, deleteCategory)
+
+//Wishlist routes
+router.get("/wishlists", isAdmin, getAllWishlists)
