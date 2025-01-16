aa2b7ec31d8d437afc077b4c47c7821f736570ed
Andrew Yang <ayang312@gmail.com>
2025-01-16 14:59:55 -0500

    edited seed file to incorporate faker dummy products

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,31 +1,9 @@
 const prisma = require(".");
-// const { faker } = require("@faker-js/faker");
-// const bcrypt = require("bcrypt")
+const { faker } = require("@faker-js/faker");
+const bcrypt = require("bcrypt");
 
 // const seed = async () => {
 
-//   let products = [];
-
-//   for (let index = 0; index < 100; index++) {
-//     let product = {};
-//     product = {
-//       name: faker.commerce.product(),
-//       description: faker.commerce.productDescription(),
-//       price: faker.commerce.price(),
-//       quantity: faker.number.int(10, 100),
-//       skuId: faker.commerce.isbn(8),
-//       image: faker.image.url()
-//     }
-
-//     products.push(product)
-//   };
-
-//   const createProducts = await prisma.product.createManyAndReturn({
-//     data: products,
-//     select: {
-//         id: true
-//     }
-//   });
 // };
 // seed()
 //   .then(async () => await prisma.$disconnect())
@@ -70,6 +48,29 @@ async function seed() {
   });
 
   // Create some products
+  let products = [];
+
+  for (let index = 0; index < 100; index++) {
+    let product = {};
+    product = {
+      name: faker.commerce.product(),
+      description: faker.commerce.productDescription(),
+      price: faker.commerce.price(),
+      quantity: faker.number.int(10, 100),
+      skuId: faker.commerce.isbn(8),
+      image: faker.image.url(),
+    };
+
+    products.push(product);
+  }
+
+  const createProducts = await prisma.product.createManyAndReturn({
+    data: products,
+    select: {
+      id: true,
+    },
+  });
+
   const product1 = await prisma.product.create({
     data: {
       name: "Product 1",
