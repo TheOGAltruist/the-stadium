81125d5d3cd924ccd2724a40d9bf46556f740521
Andrew Yang <ayang312@gmail.com>
2025-01-16 22:28:10 -0500

    added myCartItems, addCartItem, removeCartItem, updateCartItem, deleteCart endpoints to rtk query

--- client/src/redux/user/userApi.js
+++ client/src/redux/user/userApi.js
@@ -30,7 +30,43 @@ export const userApi = rootApi.injectEndpoints({
       query: () => "/orders",
     }),
 
- 
+    // CART-RELATED endpoints
+    // Fetch cart
+    myCartItems: builder.query({
+      query: () => "/cart",
+    }),
+    // Add cart item
+    addCartItem: builder.mutation({
+      query: ({ productId, quantity }) => ({
+        url: `/cart/${productId}`,
+        method: "POST",
+        body: { quantity },
+      }),
+    }),
+    // Remove cart item
+    removeCartItem: builder.mutation({
+      query: (cartItemId) => ({
+        url: `/cart/${cartItemId}`,
+        method: "DELETE",
+      }),
+    }),
+    // Update cart item
+    updateCartItem: builder.mutation({
+      query: ({ cartItemId, quantity }) => ({
+        url: `/cart/${cartItemId}`,
+        method: "PATCH",
+        body: { quantity },
+      }),
+    }),
+    // Delete cart
+    deleteCart: builder.mutation({
+      query: () => ({
+        url: "/cart",
+        method: "DELETE",
+      }),
+    }),
+
+    
   }),
 });
 
