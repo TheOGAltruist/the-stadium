8af50fe7773f092ed366b207516fc4232fb0f0d7
Andrew Yang <ayang312@gmail.com>
2025-01-16 20:03:00 -0500

    changed baseUrl for rootApi to fetch api on render.com

--- client/src/redux/api/rootApi.js
+++ client/src/redux/api/rootApi.js
@@ -4,7 +4,7 @@ export const rootApi = createApi({
   reducerPath: "api",
   tagTypes: [],
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api",
+    baseUrl: "https://the-stadium.onrender.com/api",
     prepareHeaders: (headers) => {
       // Get the token from cookies saved from backend
       headers.set("Content-type", "application/json");
