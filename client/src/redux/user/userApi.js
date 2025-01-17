aa762b4ca00d662bdd036d656cbaab1b73ed4c20
Andrew Yang <ayang312@gmail.com>
2025-01-16 22:26:46 -0500

    added ORDER-related endpoints to rtk query

--- client/src/redux/user/userApi.js
+++ client/src/redux/user/userApi.js
@@ -1,22 +1,37 @@
 import { rootApi } from "../api/rootApi";
 
 export const userApi = rootApi.injectEndpoints({
-    endpoints: (builder) => ({
-        // Get Me profile
-        getMe: builder.query({
-            query: () => '/me',
-        }),
-        // Edit Me
-        editMe: builder.mutation({
-            query: (userData) => ({
-                url: '/me',
-                method: 'PATCH',
-                body: userData,
-            }),
-        }),
-
-
+  endpoints: (builder) => ({
+    // PROFILE-related endpoints
+    // Get Me profile
+    getMe: builder.query({
+      query: () => "/me",
     }),
+    // Edit Me
+    editMe: builder.mutation({
+      query: (userData) => ({
+        url: "/me",
+        method: "PATCH",
+        body: userData,
+      }),
+    }),
+
+    // ORDER-related endpoints
+    // Create a new order
+    newOrder: builder.mutation({
+      query: (orderData) => ({
+        url: "/orders",
+        method: "POST",
+        body: orderData,
+      }),
+    }),
+    // Fetch orders
+    myOrders: builder.query({
+      query: () => "/orders",
+    }),
+
+ 
+  }),
 });
 
 export const { useGetMeQuery, useEditMeMutation } = userApi;
