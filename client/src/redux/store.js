a84834cb0e05c0abc56e3d0bc819c5eb431b7ce9
Andrew Yang <ayang312@gmail.com>
2025-01-18 23:10:16 -0500

    added userApi.reducer and userApi.middleware to the store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,7 @@ import authReducer from "./auth/authSlice";
 import { authApi } from "./auth/authApi";
 import { productApi } from "./products/productsApi";
 import { adminApi } from "./admin/adminApi";
+import { userApi } from "./user/userApi";
 
 const store = configureStore({
   reducer: {
@@ -10,12 +11,14 @@ const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
     [adminApi.reducerPath]: adminApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       authApi.middleware,
       productApi.middleware,
-      adminApi.middleware
+      adminApi.middleware,
+      userApi.middleware
     ),
 });
 
