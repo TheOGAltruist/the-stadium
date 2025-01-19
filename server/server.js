9e20d7210e0f22b6c4158b60c5ee8a047d47d900
TheAltruist <110626971+TheOGAltruist@users.noreply.github.com>
2025-01-19 05:01:30 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 // CORS middleware
 app.use(cors({
-  origin: '*', // Allow requests from this origin
+  origin: 'http://localhost:5173', // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Specify allowed methods
   credentials: true // Allow credentials if needed
 }));
